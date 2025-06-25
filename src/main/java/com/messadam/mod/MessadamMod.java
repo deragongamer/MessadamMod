373d0864aaecb26b43a86e34c87736f74dd8222a
deragongamer <amiralizaremehrjardi1391@gmail.com>
2025-06-25 11:27:22 +0330

    Initial commit - Messadam Mod base

---
+++ src/main/java/com/messadam/mod/MessadamMod.java
@@ -0,0 +1,11 @@
+package com.messadam.mod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MessadamMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Messadam Mod Initialized!");
+        // Initialize missiles, defenses, drones, bombs, etc.
+    }
+}
